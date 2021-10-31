8055e9c55fab27088faee9c248181cad335aa63d
MotsoenengTeboho <mteboho30@gmail.com>
2021-10-31 18:02:29 +0200

    Dashboard UI design. Connecting to a database to see whether it connects. Now I have to create an image table in my database and establish the connection.

--- Image album/Pages/dashboard.aspx.designer.cs
+++ Image album/Pages/dashboard.aspx.designer.cs
@@ -42,13 +42,13 @@ namespace Image_album.Pages
         protected global::System.Web.UI.WebControls.Label lblWelcome;
 
         /// <summary>
-        /// Table1 control.
+        /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Table Table1;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
 
         /// <summary>
         /// btnLogout control.
@@ -58,5 +58,14 @@ namespace Image_album.Pages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogout;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
